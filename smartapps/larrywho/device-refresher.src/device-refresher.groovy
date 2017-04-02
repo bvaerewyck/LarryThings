08b068f5db9be6493990470809a987de5ce27d80
Larry Wilcox <larrywho@gmail.com>
2017-04-01 22:32:14 -0400

    Removed event based refresh

--- smartapps/larrywho/device-refresher.src/device-refresher.groovy
+++ smartapps/larrywho/device-refresher.src/device-refresher.groovy
@@ -11,74 +11,25 @@ definition(
 preferences {
     section ("Device(s) to refresh ...") {
         input "sensor", "capability.temperatureMeasurement", title: "Device", required: true, multiple: true
-        input "meter", "capability.powerMeter", title: "Meter", multiple: false, required: true
     }
 }
 
 def installed() {
-
     initialize()
 }
 
 def updated() {
-
     unsubscribe()
+    unschedule()
     initialize()
 }
 
 def initialize() {
-
-    // initialize outlet first time through
-    def currentTime = now()
-
-    atomicState.refreshTime = currentTime
-    atomicState.eventLock = false
-    atomicState.deadlockCounter = 0
-	
-    // subscribe to events
-    subscribe(meter, "energy", meterHandler)
+   runEvery1Minute("runApp")
 }
 
-
-
-def meterHandler(evt) {
-
-    if (atomicState.eventLock == false)
-    {
-        atomicState.eventLock = true
-        atomicState.deadlockCounter = 0
-
-        try
-        {
-            def currentTime = now()
-
-
-            if ((currentTime - atomicState.refreshTime) >= 60000)
-            {
-               log.info("calling sensor.refresh()")
-               atomicState.refreshTime = currentTime
-               sensor.refresh()
-            }
-
-        }
-        catch (e)
-        {
-            atomicState.eventLock = false
-            log.info("caught an exception: $e")
-        }
-        
-        atomicState.eventLock = false
-    }
-    else
-    {
-        atomicState.deadlockCounter = atomicState.deadlockCounter + 1
-        log.info("event lock not available")
-    }
-    
-    if (atomicState.deadlockCounter >= 5)
-    {
-        atomicState.eventLock = false
-        atomicState.deadlockCounter = 0
-        log.info("event lock reset")
-    }
+def runApp() {
+   log.info("calling sensor.refresh()")
+   sensor.refresh()
 }
+
